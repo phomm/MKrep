22cdfea400f6ab159e18c7258d784f0762b6232b
phomm <phomm@mail.ru>
2021-06-02 15:53:38 +0500

    Revert "Modified sample main.js , added time counter for events"
    This reverts commit d7f60eb63f3eed6969c3fd3e13ee6188054c1843.

--- main.js
+++ main.js
@@ -1,26 +1,5 @@
 function DOMReady() {
-    report('DOM is ready');
+    console.log('DOM is ready');
 }
 
-function docLoaded() {
-    report('Document is loaded');
-}
-
-function report(message){
-	console.log(message);
-	reportTime();
-}
-
-function formatTime(time){
-	return `${time.getUTCHours()}:${time.getMinutes()}:${time.getSeconds()}.${("00" + time.getMilliseconds().toString()).slice(-3)}`;
-}
-
-let startTime = Date.now();
-function reportTime(){
-	let timeDiff = new Date(Date.now() - startTime);
-	console.log(`time from start: ${formatTime(timeDiff)}`);
-}
-
-report("started");
 document.addEventListener("DOMContentLoaded", DOMReady);
-document.onload = docLoaded;
