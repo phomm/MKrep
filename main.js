9c709f8cd2cddc583db9a8d469791742da224ce3
phomm <phomm@mail.ru>
2021-06-02 16:04:34 +0500

    Modified sample main.js , added time counter for events

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 function DOMReady() {
-    console.log('DOM is ready');
+    report('DOM is ready');
 }
 
+function docLoaded() {
+    report('Document is loaded');
+}
+
+function report(message){
+	console.log(message);
+	reportTime();
+}
+
+function formatTime(time){
+	return `${time.getUTCHours()}:${time.getMinutes()}:${time.getSeconds()}.${("00" + time.getMilliseconds().toString()).slice(-3)}`;
+}
+
+let startTime = Date.now();
+function reportTime(){
+	let timeDiff = new Date(Date.now() - startTime);
+	console.log(`time from start: ${formatTime(timeDiff)}`);
+}
+
+report("started");
 document.addEventListener("DOMContentLoaded", DOMReady);
+document.onload = docLoaded;
